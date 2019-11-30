bef6229c6db1cd7431e3160936e0542f713cd0b9
PierreBravo <askvader@live.cl>
2019-11-30 01:57:19 -0300

    activate

---
+++ sw.js
@@ -0,0 +1,14 @@
+
+
+self.addEventListener('install', evt =>{
+    console.log('service worker has been installed');
+
+});
+
+
+
+self.addEventListener('activate', evt =>{
+    console.log('service worker has been activated');
+
+});
+
